fd41a57cb33d12c3af9a607fbc589e612b4939ca
Maxim Sinelnikov <67955139+sinelnikov-web@users.noreply.github.com>
2021-09-14 02:02:28 +0600

    Add files via upload

---
+++ Task1/Task1/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace Task1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(47, 187);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(603, 892);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Фотография";
+            this.columnHeader1.Width = 300;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Цена";
+            this.columnHeader2.Width = 90;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(692, 762);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(299, 84);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Купить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 32;
+            this.listBox1.Location = new System.Drawing.Point(692, 187);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 420);
+            this.listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 32;
+            this.listBox2.Location = new System.Drawing.Point(871, 187);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 420);
+            this.listBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(688, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Левая";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(867, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Правая";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(682, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 55);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Линзы";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(37, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(201, 55);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Оправы";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1040, 1107);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Task1/Task1/Form1.cs
@@ -0,0 +1,102 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        async private void button1_Click(object sender, EventArgs e)
+        {
+            var leftLens = listBox1.SelectedItem.ToString();
+            var rightLens = listBox2.SelectedItem.ToString();
+            var framesList = listView1.CheckedItems;
+            string message = $"Левая линза: {leftLens}\nПравая линза: {rightLens}\nОправы:";
+            int cost = 0;
+            foreach (ListViewItem frame in framesList)
+            {
+                string itemText = frame.SubItems[0].Text;
+                message += $"\n---------------------------\nНазвание: {Regex.Replace(itemText, @"[\s\n]+", " ")}\nЦена: {frame.SubItems[1].Text}";
+                cost += int.Parse(frame.SubItems[1].Text.Trim().Replace(" ", ""));
+            }
+            message += $"\n---------------------------\nОбщая стоимость: {cost}";
+            MessageBox.Show(message, caption: "Ваши покупки");
+        }
+
+        async Task<Dictionary<string, Dictionary<string, string>>> get_frames()
+        {
+            string url = "https://www.optic-city.ru/ochki_s_dioptrijami/katalog_oprav/";
+            var web = new HtmlWeb();
+            var doc = await web.LoadFromWebAsync(url);
+            var framesLinksList = doc.DocumentNode.SelectNodes("//*[@id=\"content\"]/div[2]/div/div/a");
+            var framesPhotosList = doc.DocumentNode.SelectNodes("//*[@id=\"content\"]/div[2]/div/div/a/div[2]/img");
+            var framesPriceList = doc.DocumentNode.SelectNodes("//*[@id=\"content\"]/div[2]/div/div/ul/li[2]");
+            Dictionary<string, Dictionary<string, string>> framesData = new Dictionary<string, Dictionary<string, string>>();
+            for (int i = 0; i < framesPhotosList.Count; i++)
+            {
+                Dictionary<string, string> frameData = new Dictionary<string, string>();
+                frameData.Add("photo", framesPhotosList[i].Attributes["src"].Value);
+                frameData.Add("price", framesPriceList[i].InnerText.Trim());
+                frameData.Add("url", framesLinksList[i].Attributes["href"].Value);
+                framesData.Add($"{framesLinksList[i].InnerText.Replace("&nbsp;", "").Replace("&amp;", "").Trim()}{i+1}", frameData);
+            }
+            return framesData;
+        }
+        async private Task DownloadImagesFromWeb(string adress, ImageList il)
+        {
+            System.Net.WebRequest request = System.Net.WebRequest.Create(adress);
+            System.Net.WebResponse resp = await request.GetResponseAsync();
+            System.IO.Stream respStream = resp.GetResponseStream();
+            Bitmap bmp = new Bitmap(respStream);
+            respStream.Dispose();
+
+            il.Images.Add(bmp);
+        }
+
+        async private void Form1_Load(object sender, EventArgs e)
+        {
+
+            for (int i = -10; i <= 10; i++)
+            {
+                listBox1.Items.Add(i.ToString());
+                listBox2.Items.Add(i.ToString());
+            }
+
+            Dictionary<string, Dictionary<string, string>> framesList = await get_frames();
+            ImageList imageList = new ImageList();
+            imageList.ImageSize = new Size(150, 150);
+            int count = 0;
+            foreach (var key in framesList.Keys)
+            {
+                await DownloadImagesFromWeb(framesList[key]["photo"], imageList);
+
+            }
+            listView1.GridLines = true;
+            listView1.FullRowSelect = true;
+            listView1.CheckBoxes = true;
+
+            listView1.SmallImageList = imageList;
+            foreach (var key in framesList.Keys)
+            {
+                ListViewItem item = new ListViewItem();
+                item.SubItems.Add(framesList[key]["price"]);
+                item.Text = key;
+                item.ImageIndex = count++;
+                listView1.Items.Add(item);
+            }
+        }
+    }
+}
